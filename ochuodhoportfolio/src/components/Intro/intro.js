69f72081a90c01a06679c44298980378595034a7
Ochuodho <ochuodhoderrick@gmail.com>
2024-02-22 20:52:10 +0300

    styling the intro

--- ochuodhoportfolio/src/components/Intro/intro.js
+++ ochuodhoportfolio/src/components/Intro/intro.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './intro.css';
+import bg from '../../assets/mypic.png';
+import { Link } from 'react-scroll';
+import btnImg  from'../../assets/brief.png';
 
 function intro() {
   return (
-   <section id="intro"></section>
+   <section id="intro">
+        <div className="introContent">
+            <span className="hello">Hello, </span>
+            <span className="introText">I'm <span className="introName">Derrick</span><br />Website Designer</span>
+            <p className="introPara">I am a passionate Full-Stack developer with experience in creating visually appealing websites.</p>
+            <Link><button className="btn"><img src={btnImg} alt="Hire Me" /> Hire Me </button></Link>
+
+    
+        </div>
+        <img src={bg} alt="profile" className="bg" />
+
+   </section>
+   
 
   )
 }
